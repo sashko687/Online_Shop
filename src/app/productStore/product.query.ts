1ea4c20bca37503df30436122e30928f898b101c
Olek Basarab <barcelona_chelsea@ukr.net>
2020-11-11 19:44:50 +0200

    Akita2

--- src/app/productStore/product.query.ts
+++ src/app/productStore/product.query.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { QueryEntity } from '@datorama/akita';
 import { Observable } from 'rxjs';
-import { Order } from '../admin/order.interface';
+import { Product } from '../shared/interfaces';
 import { ProductsStore, ProductState } from './product.store';
 
 @Injectable({ providedIn: 'root' })
-export class OrdersQuery extends QueryEntity<ProductState, Order> {
+export class ProductQuery extends QueryEntity<ProductState, Product> {
 	constructor(protected store: ProductsStore) {
 		super(store);
 	}
 
-	selectProducts(): Observable<Order[]> {
+	selectProducts(): Observable<Product[]> {
 		return this.selectAll();
 	}
 
